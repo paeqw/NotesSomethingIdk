8e7acda01c2939c8fc3266f64da8b851e6c05a7e
Paeqw <Paeqw12@gmail.com>
2023-03-08 21:11:04 +0100

    witam xml >>>> txt

--- NotesSomethingAAAA/NotesManager.cs
+++ NotesSomethingAAAA/NotesManager.cs
@@ -2,75 +2,77 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace NotesSomethingAAAA
 {
-    public class NotesManager
-    {
-        private readonly string notesFilePath;
-        public List<Note> Notes { get; private set; }
+	public class NotesManager
+	{
+		private readonly string notesFilePath;
+		public List<Note> Notes { get; private set; }
 
-        public NotesManager()
-        {
-            notesFilePath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData), "notes.txt");
+		public NotesManager()
+		{
+			notesFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "notes.xml");
 
-            Notes = new List<Note>();
-        }
-        
+			Notes = new List<Note>();
 
-        public Note AddNote(string title, string content, DateTime DueDate, bool isDone)
-        {
-            Note newNote = new Note(title, content, DueDate, isDone);
-            Notes.Add(newNote);
-            SaveNotes();
-            return newNote;
-        }
+		}
+		
 
-        public void LoadNotes()
-        {
-            if (File.Exists(notesFilePath))
-            {
-                List<string> lines = File.ReadAllLines(notesFilePath).ToList();
-                foreach (string line in lines)
-                {
-                    string[] parts = line.Split('|');
-                    var Title = parts[0];
-                    var Content = parts[1];
-                    DateTime DueDate = Convert.ToDateTime(parts[2]);
-                    bool isDone = Convert.ToBoolean(parts[3]);
-                    Note note = new Note(Title, Content, DueDate, isDone);
-                    Notes.Add(note);
-                }
-            }
-        }
-         
-        public void SaveNotes()
-        {
-            List<string> lines = new List<string>();
-            foreach (Note note in Notes)
-            {
-                string line = $"{note.Title}|{note.Content}|{note.DueDate}|{note.isDone}";
+		public Note AddNote(string title, string content, DateTime DueDate, bool isDone)
+		{
+			Note newNote = new Note(title, content, DueDate, isDone);
+			Notes.Add(newNote);
+			SaveNotes();
+			return newNote;
+		}
 
-                lines.Add(line);
-            }
-            File.WriteAllLines(notesFilePath, lines);
-        }
-        
-        public void DeleteNote(Note note)
-        {
-            Notes.Remove(note);
-            SaveNotes();
-        }
+		public void LoadNotes()
+		{
+			if (File.Exists(notesFilePath))
+			{
+				var notesXml = XDocument.Load(notesFilePath);
+				Notes = notesXml.Root
+					.Elements("note")
+					.Select(noteXml => new Note(
+						noteXml.Element("title").Value,
+						noteXml.Element("content").Value,
+						DateTime.Parse(noteXml.Element("dueDate").Value),
+						bool.Parse(noteXml.Element("isDone").Value)
+					))
+					.ToList();
+			}
+		}
+		 
+		public void SaveNotes()
+		{//System.Xml.Linq.
+			var notesXml = new XElement("notes",
+				Notes.Select(note => new XElement("note",
+					new XElement("title", note.Title),
+					new XElement("content", note.Content),
+					new XElement("dueDate", note.DueDate),
+					new XElement("isDone", note.isDone)
+				))
+			);
 
-        public void ModifyNote(Note note, string newTitle, string newContent,DateTime date, bool done)
-        {
-            note.Title = newTitle;
-            note.Content = newContent;
-            note.DueDate = date;
-            note.isDone = done;
-            SaveNotes();
-        }
+			notesXml.Save(notesFilePath);
+		}
+		
+		public void DeleteNote(Note note)
+		{
+			Notes.Remove(note);
+			SaveNotes();
+		}
 
-        
-    }
+		public void ModifyNote(Note note, string newTitle, string newContent,DateTime date, bool done)
+		{
+			note.Title = newTitle;
+			note.Content = newContent;
+			note.DueDate = date;
+			note.isDone = done;
+			SaveNotes();
+		}      
+	}
 }
+// tabulatory >> spacje dlatego wywalilo changes w kazdej linijce poggers
